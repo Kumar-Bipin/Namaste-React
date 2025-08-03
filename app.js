2ba70ddb6403da612e2cc36107b2b45cbe86e5e5
Bipin Kumar <Bipin.Kumar@CentricConsulting.com>
2025-08-03 17:51:23 +0530

    Episode 2

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parentDiv = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1", key: "child1" }, [
     React.createElement("h1", { key: "heading1" }, "Hello World! from React"),
